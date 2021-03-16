4774ce7aed30f6d4919c41b20afcb54753f67761
Banksyw <52453122+Banksyw@users.noreply.github.com>
2021-03-16 21:05:40 +0800

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -16,7 +16,6 @@ public class t3 {
      **/
     public static void main(String[] args) {
         int[] arr = new int[]{0, 1, 2, 3, 4, 5};
-        Arrays.sort(arr);
         sort(arr, 0, arr.length - 1);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
